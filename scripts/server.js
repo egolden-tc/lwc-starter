508b941a18a9c6e7fd743233077114c1d8087614
egolden-tc <egolden@appirio.com>
2020-05-29 17:02:12 -0400

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -7,6 +7,24 @@ const path = require('path');
 const app = express();
 app.use(helmet());
 app.use(compression());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE'
+    );
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Accept, Authorization, Content-Type, X-Requested-With, Range'
+    );
+    if (req.method === 'OPTIONS') {
+        return res.send(200);
+    } else {
+        return next();
+    }
+});
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3001;
